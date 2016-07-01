27bff145e46d0f5583423262501188e289e2c1c4
Alexandra Paxton <paxton.alexandra@gmail.com>
2016-07-01 12:46:21 -0700

    Add comments and refine plotting order

--- plotting-coupled-data.r
+++ plotting-coupled-data.r
@@ -52,58 +52,70 @@ p1turns$end[p1turns$lengths==1] = p1turns$end[p1turns$lengths==1] + .1
 
 # initialize plot
 dev.new(width=4, height=2)
+plot(NULL, 
+     xlim=c(start.frame,end.frame),
+     ylim=c(-2,2),
+     cex=1,cex.axis=1,
+     ylab="Movement", 
+     xlab="Time (in frames)")
 
-# movement (continuous): plot p0
-plot(x = 1:length(toy.convo$t),
+# speech (categorical): add p0 as cross-hatching and semi-opaque grey rectangles
+for (turn in 1:dim(p0turns)[1]) {
+  
+  # plot speech as cross-hatching
+  rect(xleft = p0turns$start[turn],
+       xright = p0turns$end[turn],
+       ytop = 5,
+       ybottom = -5,
+       col=rgb(.5,.5,.5,1),
+       lwd = .5,
+       density=20,
+       angle=45,
+       border=FALSE)
+  
+  # plot some greyscale shading over it
+  rect(xleft = p0turns$start[turn],
+       xright = p0turns$end[turn],
+       ytop = 5,
+       ybottom = -5,
+       col=rgb(.5,.5,.5,.1),
+       border=FALSE)
+}
+
+# speech (categorical): add p1 as cross-hatching and semi-opaque grey rectangles
+for (turn in 1:dim(p1turns)[1]) {
+  
+  # plot speech as cross-hatching
+  rect(xleft = p1turns$start[turn],
+       xright = p1turns$end[turn],
+       ytop = 5,
+       ybottom = -5,
+       col=rgb(.5,.5,.5,1),
+       density=20,
+       lwd = .5,
+       angle=325,border=FALSE)
+  
+  # plot some greyscale shading over it
+  rect(xleft = p1turns$start[turn],
+       xright = p1turns$end[turn],
+       ytop = 5,
+       ybottom = -5,
+       col=rgb(.25,.25,.3,.2),
+       border=FALSE)
+}
+
+# movement (continuous): plot p0 as a line
+points(x = 1:length(toy.convo$t),
      y = toy.convo$p0mov,
      col=rgb(.5,.5,.5),
-     ylim=c(-2,2),
-     xlim=c(start.frame,end.frame),
-     cex=1,cex.axis=1,
-     type='l', lwd=1,
-     ann=FALSE,mgp=c(1, .4, 0),
-     xaxt='n')
+     type='l', lwd=2
+)
 
-# movement (continuous): add p1
+# movement (continuous): add p1 as a line
 points(x = 1:length(toy.convo$t),
        y = toy.convo$p1mov,
        col=rgb(.25,.25,.3),
-       cex=.15,
-       type='l',lwd=3)
-
-# speech (categorical): p0
-for (turn in 1:dim(p0turns)[1]) {
-  rect(xleft = p0turns$start[turn],
-       xright = p0turns$end[turn],
-       ytop = 5,
-       ybottom = -5,
-       col=rgb(.5,.5,.5,1),
-       density=20,
-       angle=45,border=FALSE)
-  rect(xleft = p0turns$start[turn],
-       xright = p0turns$end[turn],
-       ytop = 5,
-       ybottom = -5,
-       col=rgb(.5,.5,.5,.2),
-       border=FALSE)
-}
-
-# speech (categorical): p1
-for (turn in 1:dim(p1turns)[1]) {
-  rect(xleft = p1turns$start[turn],
-       xright = p1turns$end[turn],
-       ytop = 5,
-       ybottom = -5,
-       col=rgb(.5,.5,.5,1),
-       density=20,
-       angle=325,border=FALSE)
-  rect(xleft = p1turns$start[turn],
-       xright = p1turns$end[turn],
-       ytop = 5,
-       ybottom = -5,
-       col=rgb(.25,.25,.3,.4),
-       border=FALSE)
-}
+       type='l',lwd=2)
 
 # save the plot
 dev.copy(png,'greyscale-figure.png')
@@ -115,26 +127,14 @@ dev.off()
 
 # initialize plot
 dev.new(width=4, height=2)
-
-# movement (continuous): plot p0
-plot(x = 1:length(toy.convo$t),
-     y = toy.convo$p0mov,
-     col=rgb(0,0,1),
-     ylim=c(-2,2),
+plot(NULL, 
      xlim=c(start.frame,end.frame),
-     cex=1,cex.axis=1,
-     type='l', lwd=2,
-     ann=FALSE,mgp=c(1, .4, 0),
-     xaxt='n')
+     ylim=c(-2,2),
+     cex=1, cex.axis=1,
+     ylab="Movement", 
+     xlab="Time (in frames)")
 
-# movement (continuous): add p1
-points(x = 1:length(toy.convo$t),
-       y = toy.convo$p1mov,
-       col=rgb(1,0,0),
-       cex=.15,
-       type='l',lwd=2)
-
-# speech (categorical): p0
+# speech (categorical): add p0 as semi-opaque colored rectangles
 for (turn in 1:dim(p0turns)[1]) {
   rect(xleft = p0turns$start[turn],
        xright = p0turns$end[turn],
@@ -144,7 +144,7 @@ for (turn in 1:dim(p0turns)[1]) {
        border=FALSE)
 }
 
-# speech (categorical): p1
+# speech (categorical): add p1 as semi-opaque colored rectangles
 for (turn in 1:dim(p1turns)[1]) {
   rect(xleft = p1turns$start[turn],
        xright = p1turns$end[turn],
@@ -153,6 +153,20 @@ for (turn in 1:dim(p1turns)[1]) {
        col=rgb(1,0,0,.15),
        border=FALSE)
 }
+
+# movement (continuous): add p0 as a line
+points(x = 1:length(toy.convo$t),
+       y = toy.convo$p0mov,
+       col=rgb(0,0,1),
+       type='l', lwd=1.5
+)
+
+# movement (continuous): add p1 as a line
+points(x = 1:length(toy.convo$t),
+       y = toy.convo$p1mov,
+       col=rgb(1,0,0),
+       type='l',lwd=1.5
+)
 
 # save the plot
 dev.copy(png,'color-figure.png')
